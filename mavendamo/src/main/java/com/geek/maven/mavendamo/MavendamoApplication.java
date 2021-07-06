36a7dba1d8ea849d3698538b7a3c263f610702a2
G.E.E.K <329763867@qq.com>
2021-07-06 22:07:36 +0800

    Commit Message

---
+++ mavendamo/src/main/java/com/geek/maven/mavendamo/MavendamoApplication.java
@@ -0,0 +1,13 @@
+package com.geek.maven.mavendamo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavendamoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavendamoApplication.class, args);
+    }
+
+}
---
+++ mavendamo/src/test/java/com/geek/maven/mavendamo/MavendamoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geek.maven.mavendamo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavendamoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
